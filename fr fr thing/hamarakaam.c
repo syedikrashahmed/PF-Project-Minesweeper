fc7520289b870ee3b607742fb00d2b023c4459cf
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2024-11-14 12:14:51 +0500

    Update hamarakaam.c

--- fr fr thing/hamarakaam.c
+++ fr fr thing/hamarakaam.c
@@ -1,14 +1,18 @@
 #include "raylib.h"
+#include <stdio.h>
+#include <time.h>
 
 typedef struct Cell
 {
 	int x;
 	int y;
-	bool mine;
+	bool bomb;
 	bool revealed;
 	bool flagged;
-	int nearbyMines;
-} Cell;
+	int nearbyBombs;
+} cell;
+
+cell grid[10][10];
 
 int const cols = 10;
 int const rows = 10;
@@ -22,7 +26,10 @@ void GridInitialize();
 
 int main(void)
 {
+
     InitWindow(screenSize, screenSize, "MINESWEEPER");
+  
+
     SetTargetFPS(60);               
 
     while (!WindowShouldClose())   
@@ -35,18 +42,51 @@ int main(void)
         EndDrawing();
     }
 
-    CloseWindow();      
+
+    CloseWindow();  
+
+    
     return 0;
 }
 
+
+//x and y is array position(row and column)
 void GridInitialize()
 {
     for (int x = 0; x < cellSize; x ++) 
     {
         for (int y = 0; y < cellSize; y ++) 
-        {          
+        {
+            
             DrawRectangleLines(x*cellSize, y*cellSize, cellSize, cellSize, WHITE);
             //taake screen ke har pixel pe lines na banen, cell size ke gap se banen
         }
-    }    
+    }
+    
+    for(int i = 0; i < 10; i ++)
+    {
+        for (int j = 0; j < 10; j ++) 
+        {
+            grid[i][j].x = i;
+            grid[i][j].y = j;
+            grid[i][j].bomb = false; 
+            grid[i][j].revealed = false;
+            grid[i][j].flagged = false;
+            grid[i][j].nearbyBombs = -1;
+        }
+    }
+    
+    //bombs idhar lagayen ge 
+    int bombsToLaga = 20;
+    while(bombsToLaga > 0)
+    {
+        srand(time(0));
+        int i = rand() % 10;
+        int j = rand() % 10;
+        if (grid[i][j].bomb == false)
+        {
+            grid[i][j].bomb = true;
+            bombsToLaga--;
+        }
+    }
 }
