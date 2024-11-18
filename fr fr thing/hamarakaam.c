baf8b3e2601c6f8f059dca7322c05757a353a0b2
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2024-11-18 11:35:33 +0500

    Update hamarakaam.c

--- fr fr thing/hamarakaam.c
+++ fr fr thing/hamarakaam.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 typedef struct Cell
 {
@@ -30,7 +31,8 @@ int main(void)
     InitWindow(screenSize, screenSize, "MINESWEEPER");
   
 
-    SetTargetFPS(60);               
+    SetTargetFPS(60);   
+    srand(time(0));    
 
     while (!WindowShouldClose())   
     {
@@ -52,16 +54,10 @@ int main(void)
 
 //x and y is array position(row and column)
 void GridInitialize()
-/*
-1) lines draw horai
-2)initialization of class
-3)bombs lagao
-4) nearby bombs count 
-*/
 {
-    for (int x = 0; x < cellSize; x ++) 
+    for (int x = 0; x < 10; x ++) 
     {
-        for (int y = 0; y < cellSize; y ++) 
+        for (int y = 0; y < 10; y ++) 
         {
             
             DrawRectangleLines(x*cellSize, y*cellSize, cellSize, cellSize, WHITE);
@@ -86,47 +82,13 @@ void GridInitialize()
     int bombsToLaga = 20;
     while(bombsToLaga > 0)
     {
-        srand(time(0));
+        
         int i = rand() % 10;
         int j = rand() % 10;
         if (grid[i][j].bomb == false)
         {
             grid[i][j].bomb = true;
             bombsToLaga--;
-        }
-    }
-    
-    //count mines in nearby boxes (loop runs for all boxes in the grid)
-    for(int i = 0; i<10; i++)
-    {
-        for (int j = 0; j<10; j++)
-        {
-            int count = 0; //grid ke har box ko check karte huey usska inital count will be 0
-            if (grid[i][j].bomb == false)
-            {
-                //if bomb nai hai, count aaju baaju waale tiles mei bombs
-                for (int iOff = -1; iOff <= 1; iOff++)
-                {
-                    for (int jOff = -1; jOff <= 1; jOff++)
-                    {
-                        if (iOff == 0 && jOff == 0) //jis box ke around check kar rae hain, usse count nai karen ge
-                        {
-                            continue; 
-                        }
-
-                        if (!IndexIsValid(i + iOff, j + jOff)) // function defined later to check ke index out of bounds tou nai hai
-                        {
-                            continue;
-                        }
-
-                        if (grid[i + iOff][j + jOff].bomb)
-                        {
-                            count++;
-                        }
-                    }
-                }
-            }
-            grid[i][j].nearbyBombs = count;
         }
     }
 }
